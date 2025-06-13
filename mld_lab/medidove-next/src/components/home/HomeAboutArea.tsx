268dddad6ce5f033934b1e518179be0716d8fe85
pavan <pavansainaidu37@gmail.com>
2025-06-13 11:03:15 +0530

    updated styles

--- mld_lab/medidove-next/src/components/home/HomeAboutArea.tsx
+++ mld_lab/medidove-next/src/components/home/HomeAboutArea.tsx
@@ -13,7 +13,7 @@ interface DataType {
 }
  
 const about_video_content: DataType = {
-  title: "Who We Are",
+  title: "",
   sm_des: "Mahalakshmi Dental Laboratory is a full service lab located in Hyderabad. Since opening in November 28, 2003, we have been providing the dental community with unsurpassed dental restorations and services. We believe there are no shortcuts in producing a high quality dental restoration. This means using the best materials, the most talented technicians, and the newest technologies available.",
   sm_des_2: "We introduce ourselves as a leading Dental Laboratory of Telangana State having more than 15 years of experience in Dental Laboratory Works. Mahalakshmi Dental Lab, a very famous name in the cosmetic dentistry backed by qualified professionals trained in the various fields of dentistry by the world known companies like VITA, Ivoclar Materials of Germany, and therefore can provide quality of International Standards to all our customers.",
 }
